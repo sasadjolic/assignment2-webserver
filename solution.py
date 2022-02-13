b2f2222b4d05093e390b74c58fe26744e94be397
Sasha Jolich <sasha@ctoatlarge.com>
2022-02-13 12:16:16 -0800

    completed 200 OK and 404 Not Found

--- solution.py
+++ solution.py
@@ -9,24 +9,30 @@ def webServer(port=13331):
   #Prepare a server socket
   serverSocket.bind(("", port))
   #Fill in start
-
+  serverSocket.listen(0)
   #Fill in end
 
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.makefile().readline()
+        #print(message)
         filename = message.split()[1]
+        #print(filename)
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = f.read()
+        f.close()
         
         #Send one HTTP header line into socket.
         #Fill in start
-
+        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+        connectionSocket.send("Content-Length: {}\r\n".format(len(outputdata)).encode())
+        connectionSocket.send("Content-Type: text/html\r\n".encode())
+        connectionSocket.send("\r\n".encode())
         #Fill in end
 
         #Send the content of the requested file to the client
@@ -38,14 +44,15 @@ def webServer(port=13331):
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+        connectionSocket.send("\r\n".encode())
         #Fill in end
-
 
         #Close client socket
         #Fill in start
-
+        connectionSocket.close()
         #Fill in end
+        pass
 
     except (ConnectionResetError, BrokenPipeError):
       pass
